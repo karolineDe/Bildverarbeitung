80654283d1c49eb84562a5da7782af29112fc4d8
Karoline <Karoline@KaroLappi>
2015-11-20 10:34:19 +0100

    added TaskB threaded version

--- src/ue2/exe/MainUE2.java
+++ src/ue2/exe/MainUE2.java
@@ -8,16 +8,31 @@ import java.security.InvalidParameterException;
 
 import javax.media.jai.PlanarImage;
 
+import ue2.filters.RegionOfInterrestFilter;
 import ue2.filters.ViewImageFilter;
 import ue2.helpers.ImageSaver;
+import ue2.pipes.BufferedSyncPipe;
 import ue2.pipes.ImageStreamSupplierPipe;
 
 public class MainUE2 {
 	
 	public static void main(String[] args) {
 	
+		//TODO: Zeitmessung der verschiedenen Tasks
+		
+		runTaskAPull();
+		
+	//	runTaskAPush();
+		
+		runTaskB();
+		
+	}
+	
+	private static void runTaskAPull(){
+		
 		/** source: image supplier pipe **/
 		ImageStreamSupplierPipe imageStreamSupplierPipe0 = new ImageStreamSupplierPipe("loetstellen.jpg");
+		
 		/** point to save ROI origin **/
 		Point originOfROI = new Point(40,50);
 		
@@ -27,9 +42,6 @@ public class MainUE2 {
 			viewImageFilter = new ViewImageFilter(imageStreamSupplierPipe0);
 			
 		} catch (InvalidParameterException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (StreamCorruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
@@ -55,38 +67,85 @@ public class MainUE2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+//		try {
+//		
+//		PlanarImage image = viewImageFilter.read();
+//		ImageSaver.save(image, "ViewImageFilter1");
+//		
+//		
+//		
+//	
+//	} catch (StreamCorruptedException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
 
-        /* Achtung: Pixel oben links ausgeschnittenen Bildes hat wieder die Koordinaten 0,0. 
-         * Sie müssen noch etwas tun, damit Sie die Position dieses Pixels im Originalbild mitspeichern 
-         * im Bild (der letzte Filter braucht das!)
-        Option für den Benutzer: zeige das Rechteck in weiss mit dem Ausgangsbild */
+    /* Achtung: Pixel oben links ausgeschnittenen Bildes hat wieder die Koordinaten 0,0. 
+     * Sie müssen noch etwas tun, damit Sie die Position dieses Pixels im Originalbild mitspeichern 
+     * im Bild (der letzte Filter braucht das!)
+    Option für den Benutzer: zeige das Rechteck in weiss mit dem Ausgangsbild */
 
-        /*********** 3. einen Operator zur Bildsegmentierung auswählen: Threshold Operator /*
+    /*********** 3. einen Operator zur Bildsegmentierung auswählen: Threshold Operator /*
 
-        /*********** 3a. Parameterwerte des Operators wählen */
+    /*********** 3a. Parameterwerte des Operators wählen */
 
-        /*********** 4. beseitige lokale Störungen (z.B. schwarzer Fleck im 2. Anschluss von rechts) */
+    /*********** 4. beseitige lokale Störungen (z.B. schwarzer Fleck im 2. Anschluss von rechts) */
 
-        /*********** 4a.wähle Parameter des Filters: Größe der Maske zur Medianberechnung */
+    /*********** 4a.wähle Parameter des Filters: Größe der Maske zur Medianberechnung */
 
-        /*********** 5. nun bleiben noch die Kabelanschlüsse der „balls“; man nutzt die Kreisform der Balls aus und
-        benutzt einen Opening-Operator mit kreisförmiger Maske (in JAI: "erode" und „dilate“): */
+    /*********** 5. nun bleiben noch die Kabelanschlüsse der „balls“; man nutzt die Kreisform der Balls aus und
+    benutzt einen Opening-Operator mit kreisförmiger Maske (in JAI: "erode" und „dilate“): */
 
-        /**********  5a. wähle Parameter des Operators: Größe der Maske (Alternative: laufe mehrmals mit dem Operator
-        über das Bild) */
+    /**********  5a. wähle Parameter des Operators: Größe der Maske (Alternative: laufe mehrmals mit dem Operator
+    über das Bild) */
 
-        /********** 6.Resultatbild (ein Bild, in dem nur die „balls“ als Scheiben zu sehen sind.)in einer Datei abspeichern,
-        aber nicht als Sink realisieren, sondern nach der Abspeicherung das unveränderte Bild weiterleiten. */
+    /********** 6.Resultatbild (ein Bild, in dem nur die „balls“ als Scheiben zu sehen sind.)in einer Datei abspeichern,
+    aber nicht als Sink realisieren, sondern nach der Abspeicherung das unveränderte Bild weiterleiten. */
 
-        /********** 7.ie Scheiben zählen, ihre Zentren (Centroid, siehe unten) bestimmen, und prüfen, ob sie im
-         Toleranzbereich der Qualitätskontrolle liegen. Letztere Information wird bei Erzeugung des Filters im
-         "main" als Initialisierungsdaten an das Filterobjekt übergeben. Resultat in eine Datei schreiben. */
+    /********** 7.ie Scheiben zählen, ihre Zentren (Centroid, siehe unten) bestimmen, und prüfen, ob sie im
+     Toleranzbereich der Qualitätskontrolle liegen. Letztere Information wird bei Erzeugung des Filters im
+     "main" als Initialisierungsdaten an das Filterobjekt übergeben. Resultat in eine Datei schreiben. */
 
-        /* THREADMODEL  */
-//        Thread aThread = new Thread({
-//                PipedInputStream pis = new PipedInputStream();
+    /* THREADMODEL  */
+//    Thread aThread = new Thread({
+//            PipedInputStream pis = new PipedInputStream();
 //
-//        }
+//    }
+		
+		
+	}
+	
+	private static void runTaskAPush(){
+		
+		//TODO:
+		
+	}
+	
+	/**
+	 * Threaded Task
+	 */
+	private static void runTaskB(){
+		
+		/**Pipes**/
+		/** source: image supplier pipe **/
+		ImageStreamSupplierPipe imageStreamSupplierPipe0 = new ImageStreamSupplierPipe("loetstellen.jpg");
+		BufferedSyncPipe<PlanarImage> endOfViewPipe = new BufferedSyncPipe<>(1);
+		BufferedSyncPipe<PlanarImage> threshholdPipe = new BufferedSyncPipe<>(1);
+		
+		/** point to save ROI origin **/
+		Point originOfROI = new Point(40,50);
+		
+        /*********** 1. das Bild laden und visualisieren */	
+		new Thread(
+		new ViewImageFilter(imageStreamSupplierPipe0, endOfViewPipe)
+		).start();
+				
+        /*********** 2. eine ROI (region of interest1) definieren */
+        /* Rectangle, das relevanten Bereich umschliesst: 
+		 * x= 40, y= 50, width= 390, height= 60 */
+		Rectangle rectangle = new Rectangle(40,50,390,60);				
+		new RegionOfInterrestFilter(endOfViewPipe, threshholdPipe, rectangle);
 		
 	}
 }
--- src/ue2/filters/ViewImageFilter.java
+++ src/ue2/filters/ViewImageFilter.java
@@ -3,6 +3,7 @@ package ue2.filters;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.image.RenderedImage;
 import java.io.StreamCorruptedException;
 import java.security.InvalidParameterException;
 
@@ -14,6 +15,7 @@ import com.sun.media.jai.widget.DisplayJAI;
 
 import ue2.filters.DataTransformationFilter;
 import ue2.helpers.ImageSaver;
+import ue2.pipes.ImageStreamSupplierPipe;
 import interfaces.Readable;
 import interfaces.Writeable;
 
@@ -23,24 +25,31 @@ import interfaces.Writeable;
  */
 public class ViewImageFilter  extends DataTransformationFilter<PlanarImage>{
 
-	PlanarImage _image;
+	Readable<PlanarImage> _image;
 	
-	public ViewImageFilter(Readable<PlanarImage> input) throws InvalidParameterException, StreamCorruptedException {
+	public ViewImageFilter(Readable<PlanarImage> input, Writeable<PlanarImage> output){
+		super(input, output);
+		_image = input;
+	}
+
+	public ViewImageFilter(Readable<PlanarImage> input) {
 		super(input);
-		_image = input.read();
+		_image = input;
 	}
 
 	@Override
 	protected void process(PlanarImage entity) {
 		
+		ImageSaver.save(entity, "ViewImageFilter");
+		
 		// Create a frame for display.
-		 JFrame frame = new JFrame();
+		 JFrame frame = new JFrame("ViewImageFilter");
 		 
 		 Container contentPane = frame.getContentPane();
 		 contentPane.setLayout(new BorderLayout());
 		 
 		 // Create an instance of DisplayJAI.
-		 DisplayJAI dj = new DisplayJAI(_image);
+		 DisplayJAI dj = new DisplayJAI((RenderedImage) entity);
 		 
 		 contentPane.add(new JScrollPane(dj),BorderLayout.CENTER);
 		 
