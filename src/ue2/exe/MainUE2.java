1c2e1b5f460f682bb2666baa13c4a76209c6e247
Karoline <Karoline@KaroLappi>
2015-11-22 20:25:07 +0100

    medianfilter in TaskB

--- src/ue2/exe/MainUE2.java
+++ src/ue2/exe/MainUE2.java
@@ -3,16 +3,10 @@ package ue2.exe;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.RenderedImage;
-import java.awt.image.renderable.ParameterBlock;
-import java.awt.image.renderable.RenderableImage;
-import java.awt.image.renderable.RenderableImageOp;
 import java.io.StreamCorruptedException;
 import java.security.InvalidParameterException;
 
-import javax.media.jai.JAI;
 import javax.media.jai.PlanarImage;
-import javax.media.jai.RenderedOp;
-import javax.media.jai.operator.ThresholdDescriptor;
 
 import ue2.filters.MedianFilter;
 import ue2.filters.RegionOfInterestFilter;
@@ -42,9 +36,7 @@ public class MainUE2 {
 
 		// runTaskAPush();
 
-		// runTaskB();
-
-		// runTaskB();
+		 //runTaskB();
 
 	}
 
@@ -86,6 +78,7 @@ public class MainUE2 {
 				.wrapRenderedImage((RenderedImage) image.getAsBufferedImage(roiRectangle, image.getColorModel()));
 		ImageSaver.save(image, roiFilter);
 		ImageViewer.show(image, roiFilter);
+		
 
 		/***********
 		 * 3. einen Operator zur Bildsegmentierung auswählen: Threshold Operator
@@ -149,18 +142,19 @@ public class MainUE2 {
 	 * Threaded Task
 	 */
 	private static void runTaskB() {
-
-		PlanarImage image;
-
+		
 		/** Pipes **/
 		/** source: image supplier pipe **/
 		ImageStreamSupplierPipe imageStreamSupplierPipe = new ImageStreamSupplierPipe("loetstellen.jpg");
 		BufferedSyncPipe<PlanarImage> endOfViewPipe = new BufferedSyncPipe<>(1);
 		BufferedSyncPipe<PlanarImage> thresholdPipe = new BufferedSyncPipe<>(1);
 		BufferedSyncPipe<PlanarImage> searchMedianPipe = new BufferedSyncPipe<>(1);
+		BufferedSyncPipe<PlanarImage> ballPipe = new BufferedSyncPipe<>(1);
 
 		/*********** 1. das Bild laden und speichern */
-		new Thread(new ViewImageFilter(imageStreamSupplierPipe, endOfViewPipe)).start();
+		new Thread(
+				new ViewImageFilter(imageStreamSupplierPipe, endOfViewPipe)
+		).start();
 
 		try {
 			ImageViewer.show(imageStreamSupplierPipe.read(), "ViewImageFilter");
@@ -178,10 +172,55 @@ public class MainUE2 {
 		 */
 		Rectangle roiRectangle = new Rectangle(40, 50, 390, 60);
 
-		new Thread(new RegionOfInterestFilter(imageStreamSupplierPipe, thresholdPipe, roiRectangle)).start();
+		new Thread(
+				new RegionOfInterestFilter(imageStreamSupplierPipe, thresholdPipe, roiRectangle)
+		).start();
 
 		/************* 3. Bildsegmentierung Threshold Operator ************/
-		double[][] thresholdParameters = { new double[] { 0 }, new double[] { 25 }, new double[] { 255 } };
-		new Thread(new ThresholdFilter(thresholdPipe, searchMedianPipe, thresholdParameters)).start();
+		double[][] thresholdParameters = { new double[] { 0 }, new double[] { 28 }, new double[] { 255 } };
+		new Thread(
+				new ThresholdFilter(thresholdPipe, searchMedianPipe, thresholdParameters)
+		).start();
+		
+		/***********
+		 * 4. beseitige lokale Störungen (z.B. schwarzer Fleck im 2. Anschluss
+		 * von rechts) 
+		 * 4a.wähle Parameter des Filters: Größe der Maske zur
+		 * Medianberechnung
+		 */
+		Integer maskSize = 6;
+		
+		new Thread(
+				new MedianFilter(searchMedianPipe, ballPipe, maskSize)
+		).start();
+		
+		/***********
+		 * 5. nun bleiben noch die Kabelanschlüsse der „balls“; man nutzt die
+		 * Kreisform der Balls aus und benutzt einen Opening-Operator mit
+		 * kreisförmiger Maske (in JAI: "erode" und „dilate“):
+		 *
+		 * 5a. wähle Parameter des Operators: Größe der Maske (Alternative:
+		 * laufe mehrmals mit dem Operator über das Bild)
+		 */
+
+		
+		
+		/**********
+		 * 6.Resultatbild (ein Bild, in dem nur die „balls“ als Scheiben zu
+		 * sehen sind.)in einer Datei abspeichern, aber nicht als Sink
+		 * realisieren, sondern nach der Abspeicherung das unveränderte Bild
+		 * weiterleiten.
+		 */
+		
+		
+
+		/**********
+		 * 7.Scheiben zählen, ihre Zentren (Centroid, siehe unten) bestimmen,
+		 * und prüfen, ob sie im Toleranzbereich der Qualitätskontrolle liegen.
+		 * Letztere Information wird bei Erzeugung des Filters im "main" als
+		 * Initialisierungsdaten an das Filterobjekt übergeben. Resultat in eine
+		 * txt Datei schreiben.
+		 */
+		 
 	}
 }
